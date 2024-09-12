b1a7fef1ba1354b798dd6cd94f93e81dda558c4f
CrisCamUO <crisocanau@gmail.com>
2024-09-12 11:53:40 -0500

    Abordamos tipos de datos

---
+++ tipos-de-datos.py
@@ -0,0 +1,17 @@
+"String" #String se utiliza para cadenas de texto
+'String' #Así tambié podemos generar un String
+"Hola Mundo"
+"Hello World"
+
+"""Tus datos son:
+    nombre: Crsithian 
+    apellido: Unas""" #Para envuelve en u bloque
+'''Tus datos son:
+    nombre: Cristhian
+    apellido: Unas''' #Igualmente envuelve en un bloque
+#En phyton tenemos diferetnes tipos de números
+3 #Número entero
+30.2 #Número tipo float
+#También tenemos el tipo de datos boolean
+True #Verdadero
+False #Falso
