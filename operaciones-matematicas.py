2a7efbdd5d74432d2fdcd3c83baba9415d25d65b
CrisCamUO <crisocanau@gmail.com>
2024-09-21 10:46:43 -0500

    Realizando operaciones matematicas en python

---
+++ operaciones-matematicas.py
@@ -0,0 +1,30 @@
+#Operadores numéricos
+a = 10
+b = 3
+print("Suma: ", a + b)
+print("Resta: ", a - b)
+print("Multiplicación:", a * b)
+print("Potenciación: ", a**b)
+print("División: ", a / b)
+#En python, el operador de módulo % se utiliza para obtener l residuo de una división entre dos números
+print("Modulo: ", a % b)
+#División de parte enter
+print("División entera: ", a//b)
+#Hacer artificios
+a += 2
+print(a)
+a *= 2
+print(a)
+a /= 2
+print(a)
+#Jerarquía de operacione (), ^, * or /, + or -
+operation_1 = 2 + 3 * 4
+operation_1_2 = 2 + (3 * 4)
+print(operation_1)
+print(operation_1_2)
+operation_2 = 2 + 3 * 4
+operation_2_2 = (2 + 3) * 4
+print(operation_2)
+print(operation_2_2)
+operation_3 = (2 + 3) * (4**2) / 8 - 1
+print(operation_3)
